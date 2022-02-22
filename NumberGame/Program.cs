6b6c6b97d096a758d22af9ec56ae7c4cf5915ff0
Yeasin <realalyeasin@gmail.com>
2022-02-22 23:51:10 +0600

    scratch

--- NumberGame/Program.cs
+++ NumberGame/Program.cs
@@ -6,6 +6,26 @@ namespace NumberGame
     {
         static void Main(String[] args)
         {
+            string appName = "NumberiT";
+            string appVersion = "1.0.0";
+            string author = "Al Yeasin";
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, author);
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("What is your name?");
+            
+            string input = Console.ReadLine();
+            Console.WriteLine("Welcome {0}, let's play a game . . .", input);
+            Console.WriteLine();
+
+            int correctNumber = 9;
+
+            int guess = 0;
+            Console.WriteLine("Guess a number between 1 and 10");
 
         }
     }
