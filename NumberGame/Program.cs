a7c6748f40d04eac71f42119316731b144e50a8e
Yeasin <realalyeasin@gmail.com>
2022-02-23 02:10:44 +0600

    complete

--- NumberGame/Program.cs
+++ NumberGame/Program.cs
@@ -18,15 +18,71 @@ namespace NumberGame
             Console.WriteLine();
             Console.WriteLine("What is your name?");
             
-            string input = Console.ReadLine();
-            Console.WriteLine("Welcome {0}, let's play a game . . .", input);
+            string inputName = Console.ReadLine();
+            Console.WriteLine("Welcome {0}, let's play a game . . .", inputName);
             Console.WriteLine();
 
-            int correctNumber = 9;
+            while (true)
+            {
+                Random random = new Random();
 
-            int guess = 0;
-            Console.WriteLine("Guess a number between 1 and 10");
+                int correctNumber = random.Next(1, 20);
 
+                int guess = 0;
+                Console.WriteLine("Guess a number between 1 and 20");
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Sorry! It's not a number");
+
+                        Console.ResetColor();
+                    }
+
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Wrong Number, Try Again");
+
+                        Console.ResetColor();
+                    }
+
+                    if (guess == correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.WriteLine("Correct Number");
+
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.WriteLine("Play Again? [Y or N]");
+
+                        string ans = Console.ReadLine().ToUpper();
+
+                        if (ans == "Y")
+                        {
+                            continue;
+                        }
+                        else if (ans == "N")
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
